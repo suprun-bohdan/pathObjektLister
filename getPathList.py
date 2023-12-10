03d65e2882191767438b161fd080fc26b8cb3ed2
Bohdan <Bohdan Suprun>
2023-12-10 18:31:07 +0000

    Fixed sort

--- getPathList.py
+++ getPathList.py
@@ -1,10 +1,25 @@
 import os
 import sys
+import re
+
+def sort_key(name):
+    # Знаходимо числа на початку назви файлу
+    match = re.match(r'(\d+)', name)
+    # Якщо є число, повертаємо його як ключ для сортування
+    if match:
+        return int(match.group(1))
+    # Інакше повертаємо саму назву
+    return name
 
 def list_files(directory):
     try:
-        # Перелічення файлів і папок у вказаній директорії
-        for file in os.listdir(directory):
+        # List files and folders in the directory
+        files = os.listdir(directory)
+        # Sort using the custom sort key
+        files.sort(key=sort_key)
+
+        # Print sorted files
+        for file in files:
             print(file)
     except FileNotFoundError:
         print(f"Directory not found: {directory}")
@@ -13,6 +28,6 @@ def list_files(directory):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Using: python getPathList.py [шлях до директорії]")
+        print("Using: python getPathList.py [path to directory]")
     else:
         list_files(sys.argv[1])
